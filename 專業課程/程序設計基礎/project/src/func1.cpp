5b36f982f803a7fc5771c7ccbf2cc7833392db4b
VenchiJiang <57556104+VenchiJiang@users.noreply.github.com>
2024-12-31 18:50:35 +0800

    Delete 專業課程/程序設計基礎 directory

--- 專業課程/程序設計基礎/project/src/func1.cpp
+++
@@ -1,8 +0,0 @@
-
-//functions on this page-------------//
-double func1(double);
-//-----------------------------------//
-
-double func1(double x) {
-    return x;
-}
--- 專業課程/程序設計基礎/project/src/func2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-//functions on this page-------------//
-double func2(double);
-//-----------------------------------//
-
-double func2(double x) {
-    return x * x;
-}
